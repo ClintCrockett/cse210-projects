ece2808498c6c681818b4a8aef43c3521d9536d5
Jayton Crockett <clint.crockett88@gmail.com>
2023-01-06 13:22:07 -0700

    Prep 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your current grade percent? ");
+        string answer = Console.ReadLine();
+        double percent = int.Parse(answer);
+        
+        string value = "";
+
+        if (percent >= 90){
+            if (percent <= 93){
+                value += "A";
+            }
+            else{
+                value += "A-";
+            }
+            Console.Write($"You currently have an {value} in the class.");
+        }
+        else if (percent >= 80){
+            if (percent >= 87){
+                value += "B+";
+            }
+            else if (percent >= 83){
+                value += "B";
+            }
+            else{
+                value += "B-";
+            }
+            Console.Write($"You currently have a {value}");
+        }
+        else if (percent >= 70){
+            if (percent >= 77){
+                value += "C+";
+            }
+            else if (percent >= 73){
+                value += "C";
+            }
+            else{
+                value += "C-";
+            }
+            Console.Write($"You currently have a {value}");
+        }
+        else{
+            value += "F";
+            Console.Write($"You currently have a {value}");
+        }
     }
 }
