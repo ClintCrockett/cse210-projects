ddfaeb51c210f7112a52ccbb6e3799e29523c906
Austin Reeves <ree21020@byui.edu>
2023-03-10 22:39:22 -0500

    First work on Week10 Develop

---
+++ prove/Develop05/goals.cs
@@ -0,0 +1,19 @@
+
+
+
+
+class goals {
+    protected string _name = "";
+    protected string _description = "";
+    protected int _points = -1;
+    protected bool _isComplete = false;
+
+
+    public virtual void setComplete(){
+       
+        _isComplete = true;
+    }
+
+
+
+}
---
+++ prove/Develop05/simple.cs
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+class simple : goals {
+
+    public simple(){
+
+        _points = 100;
+
+        // somehow adds points to the tally in the main program class
+
+    }
+
+    
+
+
+}
